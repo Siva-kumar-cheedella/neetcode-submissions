062298fdfd92cfddeb4a36f7cf8064db0e95598c
Siva-kumar-cheedella <86705861+Siva-kumar-cheedella@users.noreply.github.com>
2026-08-28 19:52:33 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,45 @@
+class MinStack {
+public:
+    stack<long> st;
+    
+    int curMin;
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.size() == 0){
+            curMin = val;
+            st.push(0);
+        }else{
+            st.push(val - curMin);
+            // cout<<"pushed: " << val-curMin << endl;
+            if(val < curMin)
+            {
+                // cout<<"CurMin: " << val<<endl;
+                curMin = val;
+            }
+        }
+        
+       
+    }
+    
+    void pop() {
+        long top = st.top();
+        // cout<< "top: " << top << endl;
+        if(top < 0){
+            curMin = curMin - top;
+            // cout<< "updated curMin: " << curMin << endl;
+        }
+        st.pop();
+    }
+    
+    long top() {
+       if(st.top() >= 0 ) return st.top() + curMin;
+       return curMin;
+    }
+    
+    int getMin() {
+        return curMin;
+    }
+};
